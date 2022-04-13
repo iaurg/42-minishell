cc81de215967cb711920125a5d796541322dc215
Victor Wildner <vcwildm@gmail.com>
2022-04-13 08:42:13 -0300

    feat: create shell prefix

--- minishell/src/minishell.c
+++ minishell/src/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 23:07:33 by itaureli          #+#    #+#             */
-/*   Updated: 2022/02/15 06:33:11 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/04/13 08:38:11 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,18 +63,37 @@ leaks
 
 */
 
-int	main(int argc, char *argv[])
+static void	shell_prefix_handler(int signal)
 {
+	char cwd[1025];
+
+	if (signal == SIGINT)
+	{
+		getcwd(cwd, 1024);
+		ft_putstr_fd("\033[2D\033[0K", STDERR_FILENO);
+		ft_putstr_fd("\n", STDERR_FILENO);
+		ft_putstr_fd("\r\033[1;31muser@minishell\033[0;0m", STDERR_FILENO);
+		ft_putstr_fd(":\033[1;34m", STDERR_FILENO);
+		ft_putstr_fd(cwd, STDERR_FILENO);
+		ft_putstr_fd("\033[0;0m$ ", STDERR_FILENO);
+	}
+}
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	char buffer[1024];
+
 	if (argc > 1 && argv)
 	{
 		print_error(NO_ARGS);
-		return (0);
+		return (1);
 	}
-	char buffer[1024];
+	pwd();
 	while (1)
 	{
+		signal(SIGINT, shell_prefix_handler);
 		if (take_input(buffer))
-			continue;
+			break;
 		parse_input(buffer);
 	}
 	return (0);
