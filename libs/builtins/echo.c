cb4ea76fbe257cd662cfa4da36d203945b654f1f
Italo A <italoaurelior@gmail.com>
2022-04-20 05:30:32 -0300

    fix: testing echo with different commands

--- libs/builtins/echo.c
+++ libs/builtins/echo.c
@@ -6,18 +6,22 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 04:54:03 by itaureli          #+#    #+#             */
-/*   Updated: 2022/04/20 05:07:57 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/04/20 05:30:06 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/builtins.h"
 
-int	echo(char **args)
+// Test command:
+// gcc -Wall -Wextra -Werror -o echo_test ./libs/builtins/echo.c ./libs/libft/*.c && ./echo_test
+int	echo(char **args, int fd)
 {
-	while (*args)
+	int i = 1;
+
+	while (args[i])
 	{
-		write(STDOUT_FILENO, *args, ft_strlen(*args));
-		write(STDOUT_FILENO, " ", 1);
+		write(fd, args[i], ft_strlen(*args));
+		write(fd, " ", 1);
 		args++;
 	}
 	return (0);
@@ -27,12 +31,14 @@ int	main(void)
 {
 	char *args[] = {
 		"echo",
+		"-n",
 		"test",
 		"test2",
 		"test3",
+		"-n",
 		NULL
 	};
 
-	echo(args);
+	echo(args, 1);
 	return (0);
 }
