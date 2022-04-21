d0fc1ab3c9ec3eb48a7177e4e4b2a454e9438d0c
Victor Wildner <vcwildm@gmail.com>
2022-04-20 21:16:33 -0300

    feat: implement builtins tests via workaround

---
+++ tests/test_builtins.py
@@ -0,0 +1,61 @@
+import unittest
+import subprocess
+from glob import glob
+
+
+class TestBuilder(unittest.TestCase):
+    test_name = None
+    @classmethod
+    def setUpClass(cls):
+        builtins_files = glob("./libs/builtins/*.c")
+        libft_files = glob("./libs/libft/*.c")
+        command = [
+            "gcc",
+            "-Wall",
+            "-Wextra",
+            "-o",
+            cls.test_name,
+            f"./tests/test_{cls.test_name}.c",
+            *builtins_files,
+            *libft_files
+        ]
+        subprocess.call(command)
+
+    @classmethod
+    def tearDownClass(cls):
+        command = ["rm", cls.test_name]
+        subprocess.call(command)
+
+
+class TestEnv(TestBuilder):
+    test_name = "env"
+
+    def test_command(self):
+        # The command under test
+        command = [f"./{self.test_name}"]
+        result = subprocess.run(command, capture_output=True).stdout
+
+        # The bash equivalent
+        bash_emulated_command = ["env"]
+        expected_result = subprocess.run(bash_emulated_command, capture_output=True).stdout
+
+        self.assertEqual(result, expected_result)
+
+
+@unittest.skip("Not implemented yet")
+class TestEcho(TestBuilder):
+    test_name = "echo"
+
+    def test_command(self):
+        # The command under test
+        command = [f"./{self.test_name}", "Hello World"]
+        result = subprocess.run(command, capture_output=True).stdout
+
+        # The bash equivalent
+        bash_emulated_command = ["echo", "Hello World"]
+        expected_result = subprocess.run(bash_emulated_command, capture_output=True).stdout
+
+        self.assertEqual(result, expected_result)
+
+if __name__ == "__main__":
+    unittest.main()
