8b707e1b244df0b0ac6ad896b0987c0baeb3b9b5
Victor Wildner <vcwildm@gmail.com>
2022-04-21 21:01:31 -0300

    feat: add export builtin

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 23:01:22 by vwildner          #+#    #+#             */
-/*   Updated: 2022/04/19 23:50:48 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/04/21 15:57:03 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@
 
 # include <unistd.h>
 # include <stdio.h>
+# include <string.h>
 # include <stdlib.h>
+# include <errno.h>
 # include "../libs/libft/libft.h"
 
 typedef enum builtin {
@@ -30,8 +32,8 @@ typedef enum builtin {
 
 typedef struct s_command {
 	int			argc;
-	char		*argv[MAX_ARGS];
-	char		*envp[MAX_ARGS];
+	char		**argv;
+	char		**envp;
 	t_builtin	builtin;
 }	t_command;
 
@@ -39,6 +41,7 @@ typedef int	t_dispatcher(t_command *cmd);
 
 /* internal functions */
 int			env(void);
+int			export(char *arg, char *envp[]);
 
 /* public interface */
 int			builtins_echo(t_command *cmd);
