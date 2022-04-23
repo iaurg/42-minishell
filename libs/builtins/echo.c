ec497a5fb0e04f74164a504146b152ff51327c8b
Italo A <italoaurelior@gmail.com>
2022-04-23 05:38:06 -0300

    feat: implement flag

--- libs/builtins/echo.c
+++ libs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 04:54:03 by itaureli          #+#    #+#             */
-/*   Updated: 2022/04/20 19:04:52 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/04/23 05:37:53 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,32 @@
 
 // Test command:
 // gcc -Wall -Wextra -Werror -o echo_test ./libs/builtins/echo.c ./libs/libft/*.c && ./echo_test
+static int	check_flag_n(char **args)
+{
+	if (ft_strncmp(args[1], "-n", 1) == 0)
+		return (1);
+	return (0);
+}
+
 int	echo(char **args, int fd)
 {
-	int i = 1;
+	int i;
+	int has_n;
 
+	i = 1;
+	has_n = check_flag_n(args);
+
+	if (has_n)
+		i++;
 	while (args[i])
 	{
 		write(fd, args[i], ft_strlen(args[i]));
-		write(fd, " ", 1);
+		if(args[i + 1])
+			write(fd, " ", 1);
 		i++;
 	}
+	if(!has_n)
+		write(fd, "\n", 1);
 	return (0);
 }
 
