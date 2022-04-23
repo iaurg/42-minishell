2d9f70a14aa691e3bb821d1c1a29ee3c33a5ee3b
Italo A <italoaurelior@gmail.com>
2022-04-23 07:50:12 -0300

    fix: refactor logic

--- libs/builtins/echo.c
+++ libs/builtins/echo.c
@@ -6,17 +6,15 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 04:54:03 by itaureli          #+#    #+#             */
-/*   Updated: 2022/04/23 05:46:16 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/04/23 07:49:39 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/builtins.h"
 
-// Test command:
-// gcc -Wall -Wextra -Werror -o echo_test ./libs/builtins/echo.c ./libs/libft/*.c && ./echo_test
-static int	check_flag_n(char **args)
+static int	check_flag_n(char *args)
 {
-	if (ft_strncmp(args[1], "-n", 1) == 0)
+	if (ft_strncmp(args, "-n", 1) == 0)
 		return (1);
 	return (0);
 }
@@ -26,20 +24,20 @@ int	echo(char **args, int fd)
 	int i;
 	int has_n;
 
-	i = 1;
-	has_n = check_flag_n(args);
+	i = 0;
+	has_n = check_flag_n(args[0]);
 
 	if (has_n)
 		i++;
 	while (args[i])
 	{
 		write(fd, args[i], ft_strlen(args[i]));
-		if(args[i + 1])
+		if (args[i + 1])
 			write(fd, " ", 1);
 		i++;
 	}
-	if(has_n)
-		write(fd, "\n", 1);
+	if (has_n)
+		return (0);
+	write(fd, "\n", 1);
 	return (0);
 }
-
