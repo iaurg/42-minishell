dd800de651ef4ff736cfbdba77cd9924164fcecc
Victor Wildner <vcwildm@gmail.com>
2022-04-27 21:30:20 -0300

    Feature: Tests for parser (#28)
    * feat: create tests for parser
    
    * feat: add python requirements

---
+++ run_test.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+# WARNING: This testing script only works for the minishell module.
+# If you want to test builtins try running `make test_builtins`
+# Usage: ./run_test.sh <test_name>
+
+cc tests/test_$1.c \
+  libs/libft/*.c src/wip_lexer.c src/readline.c src/print_error.c src/signal.c \
+  -lreadline -lrt -lm \
+  -o test_parse \
+    && ./test_$1 \
+	&& rm ./test_$1
