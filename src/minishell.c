6ec132b442bc6a7dca0a8383b01ff2002eb7ab35
Victor Wildner <vcwildm@gmail.com>
2022-05-03 22:55:51 -0300

    Feature: Minishell initial implementation (#31)
    * feat: add bitwise example
    
    * feat: add hash table example
    
    * feat: add initial shell wrapper implementation

--- src/minishell.c
+++ src/minishell.c
@@ -3,18 +3,96 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/19 23:07:33 by itaureli          #+#    #+#             */
-/*   Updated: 2022/04/16 19:12:00 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/05/03 22:48:51 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+int	execute(char *args[], char *envp[])
+{
+	pid_t		pid;
+	pid_t		wpid;
+	int			status;
+
+	pid = fork();
+	if (pid == 0)
+	{
+		if (execvp(args[0], args) == -1)
+			perror("Command not found");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid < 0)
+	{
+		perror("Error forking");
+		return (0);
+	}
+	else
+	{
+		wpid = waitpid(pid, &status, WUNTRACED);
+		while (!WIFEXITED(status) && !WIFSIGNALED(status))
+			wpid = waitpid(pid, &status, WUNTRACED);
+	}
+	return (1);
+}
+
+//void	handle_inner_arg(char *args)
+//{
+//	if (args[i] == '(')
+//	{
+//		clean_arg = strtok(args[i], ")");
+//		tmp = shell_precedence(clean_arg, envp);
+//		if (tmp)
+//		{
+//			free(args[i]);
+//			*args[i] = tmp;
+//		}
+//	}
+//}
+
+void	handle_dollar_sign(char **args, char *tmp, int i)
+{
+	if (args[i][0] == '$')
+	{
+		tmp = getenv(&args[i][1]);
+		if (tmp)
+		{
+			free(args[i]);
+			args[i] = tmp;
+		}
+	}
+}
+
+void	expand_args(char *args[], char *envp[])
+{
+	int		i;
+	char	*tmp;
+
+	i = -1;
+	tmp = NULL;
+	while (args[++i])
+	{
+		handle_dollar_sign(args, tmp, i);
+		if (args[i][0] == '~')
+		{
+			tmp = getenv("HOME");
+			if (tmp)
+			{
+				free(args[i]);
+				args[i] = ft_strjoin(tmp, &args[i][1]);
+			}
+		}
+	}
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	char	buffer[1024];
+	char	**args;
+	int		status;
 
 	if (argc > 1 && argv)
 	{
@@ -22,11 +100,14 @@ int	main(int argc, char *argv[], char *envp[])
 		return (1);
 	}
 	signal(SIGQUIT, SIG_IGN);
-	while (1)
+	status = 1;
+	while (status)
 	{
 		if (take_input(buffer))
 			break ;
-		parse_input(buffer);
+		args = parse_input(buffer);
+		expand_args(args, envp);
+		status = execute(args, envp);
 	}
 	return (0);
 }
