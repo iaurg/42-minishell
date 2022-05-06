234b85d1d698b27c6326ab3b4fbbdba1cfd226b1
Victor Wildner <vcwildm@gmail.com>
2022-05-06 18:38:49 -0300

    feat: update libft with linked list functions

--- libs/libft/libft.h
+++ libs/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/28 07:38:35 by itaureli          #+#    #+#             */
-/*   Updated: 2022/05/05 20:45:43 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/05/06 18:37:14 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,15 @@
 # include <stdlib.h>
 # include <unistd.h>
 # define BUFFER_SIZE 1
+
+/**
+ * @brief A list of nodes with content.
+ */
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
 
 char		*get_next_line(int fd);
 void		free_and_reset(void *var);
@@ -55,6 +64,95 @@ void		ft_striteri(char *s, void (*f)(unsigned int, char *));
 char		*ft_itoa(int n);
 char		**ft_split(char const *s, char c);
 char		*ft_getenv(const char *env);
-char	*ft_strtok(char *str, char *delim);
+char		*ft_strtok(char *str, char *delim);
+/**
+ * @brief Allocates (with malloc(3)) and returns a new element.
+ * The variable ’content’ is initialized with the value of the
+ * parameter ’content’. The variable ’next’ is initialized to NULL.
+ *
+ * @param content The content to create the new element with
+ * @return t_list* The new element
+ */
+t_list		*ft_lstnew(void *content);
+
+/**
+ * @brief Adds the element ’new’ at the beginning of the list.
+ *
+ * @param lst The address of a pointer to the first link of a list
+ * @param new The address of a pointer to the element to be added to the list
+ */
+void		ft_lstadd_front(t_list **lst, t_list *new);
+
+/**
+ * @brief Counts the number of elements in a list.
+ *
+ * @param lst The beginning of a list
+ * @return int Returns the length of a list
+ */
+int			ft_lstsize(t_list *lst);
+
+/**
+ * @brief Returns the last element of the list.
+ *
+ * @param lst The beginning of the list
+ * @return t_list* Returns the last element of the list.
+ * Returns NULL if `lst` is empty
+ * or is already in the last element.
+ */
+t_list		*ft_lstlast(t_list *lst);
+
+/**
+ * @brief Adds the `new` element at the end of the list.
+ *
+ * @param lst The address of a pointer to the first link of a list
+ * @param new The address of a pointer to the element to be
+ * added to the list.
+ */
+void		ft_lstadd_back(t_list **lst, t_list *new);
+
+/**
+ * @brief Takes as a parameter an element and frees the
+ * memory of the element’s content using the function
+ * ’del’ given as a parameter and free the element.
+ * The memory of ’next’ must not be freed.
+ *
+ * @param lst The element to free
+ * @param del The address of the function used to delete the content.
+ */
+void		ft_lstdelone(t_list *lst, void (*del)(void*));
+
+/**
+ * @brief Deletes and frees the given element and every
+ * successor of that element, using the function ’del’ and free(3).
+ * Finally, the pointer to the list must be set to NULL.
+ *
+ * @param lst The adress of a pointer to an element
+ * @param del The adress of the function used to delete
+ * the content of the element.
+ */
+void		ft_lstclear(t_list **lst, void (*del)(void*));
+
+/**
+ * @brief Iterates the list ’lst’ and applies the function
+ * ’f’ to the content of each element.
+ *
+ * @param lst The adress of a pointer to an element
+ * @param f The adress of the function used to iterate on the list
+ */
+void		ft_lstiter(t_list *lst, void (*f)(void *));
+
+/**
+ * @brief Iterates the list ’lst’ and applies the function ’f’
+ * to the content of each element. Creates a new list resulting
+ * of the successive applications of the function ’f’.
+ * The ’del’ function is used to delete the content of an element if needed.
+ *
+ * @param lst	The adress of a pointer to an element
+ * @param f		The adress of the function used to iterate on the list
+ * @param del	The adress of the function used to delete the
+ * content of an element if needed
+ * @return t_list* Returns the new list or NULL if the allocation fails.
+ */
+t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
