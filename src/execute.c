a1d25b653537eb25eb04a3623e062d09a4c127cc
Italo <italoaurelior@gmail.com>
2022-05-06 22:30:15 -0300

    fix: minishell empty (#37)
    * fix: minishell empty
    
    * fix: norminette

--- src/execute.c
+++ src/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/04 17:58:19 by vwildner          #+#    #+#             */
-/*   Updated: 2022/05/05 22:07:00 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/05/06 22:27:25 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,8 @@ int	execute(char *args[], char *envp[])
 	pid_t		pid;
 	int			status;
 
+	if (!args[0])
+		return (1);
 	if (try_builtins_exec(args, envp) == 0)
 		return (1);
 	pid = fork();
@@ -84,10 +86,7 @@ int	execute(char *args[], char *envp[])
 		exit(EXIT_FAILURE);
 	}
 	else if (pid < 0)
-	{
-		perror("Error forking");
 		return (0);
-	}
 	else
 	{
 		waitpid(pid, &status, WUNTRACED);
