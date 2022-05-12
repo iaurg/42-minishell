a19c76a0f26fefe185bd91a01a98714f2968b3d1
Victor Wildner <vcwildm@gmail.com>
2022-05-11 21:46:09 -0300

    feat: add colors to terminal

--- src/readline.c
+++ src/readline.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   readline.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 16:31:19 by itaureli          #+#    #+#             */
-/*   Updated: 2022/05/11 19:57:33 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/05/11 21:44:10 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,19 +42,18 @@ static char	*get_inline_shell_display(t_list *envp[])
 {
 	char	cwd[1025];
 	char	*tmp;
-	char	*other;
 	char	*path;
 	char	*tmp_user;
 
 	getcwd(cwd, 1024);
 	path = get_last_slash_arg(cwd);
+	tmp = ft_strdup("\033[1;31m");
 	tmp_user = ft_strdup(ms_getenv(envp, "USER"));
-	tmp = ft_strjoin(tmp_user, "\033[0;31m@minishell ");
-	other = ft_strjoin(tmp, path);
+	tmp = ft_strjoin(tmp, tmp_user);
+	tmp = ft_strjoin(tmp, "\033[0;36m@minishell \033[0;34m");
+	tmp = ft_strjoin(tmp, path);
+	tmp = ft_strjoin(tmp, " ~ $\033[0m ");
 	free(tmp_user);
-	free(tmp);
-	tmp = ft_strjoin(other, " ~ $\033[0m ");
-	free(other);
 	return (tmp);
 }
 
