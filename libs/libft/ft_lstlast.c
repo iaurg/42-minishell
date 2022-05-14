f794f0b9ffe7831d780930c1dfc48aa95798bb20
Italo A <italoaurelior@gmail.com>
2022-05-14 10:10:36 -0300

    refactor: lst logic on libft

--- libs/libft/ft_lstlast.c
+++ libs/libft/ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/29 18:13:17 by vwildner          #+#    #+#             */
-/*   Updated: 2022/05/12 22:25:57 by itaureli         ###   ########.fr       */
+/*   Updated: 2022/05/14 10:03:47 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ t_list	*ft_lstlast(t_list *lst)
 {
 	t_list	*tmp;
 
+	if (lst == NULL)
+		return (lst);
 	tmp = lst;
-	if (tmp == NULL)
-		return (NULL);
 	while (tmp->next != NULL)
 		tmp = tmp->next;
 	return (tmp);
