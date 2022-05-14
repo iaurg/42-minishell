a4413ebb0e6221c8178d56c742cb86e43b09b9d1
Victor Wildner <vcwildm@gmail.com>
2022-05-14 02:13:57 -0300

    feat: update tests

--- libs/builtins/cd.c
+++ libs/builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 20:31:33 by itaureli          #+#    #+#             */
-/*   Updated: 2022/05/14 01:03:30 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/05/14 01:43:06 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@
 int	set_oldpwd(t_list *envp[], char *buf)
 {
 	char	*old_pwd;
+	char	*new_pwd;
 
 	old_pwd = ft_strjoin("OLDPWD=", buf);
 	export(old_pwd, envp);
+	free(old_pwd);
+	getcwd(buf, 1024);
+	new_pwd = ft_strjoin("PWD=", buf);
+	export(new_pwd, envp);
+	free(new_pwd);
 	return (0);
 }
 
@@ -46,29 +52,16 @@ static void print_cd_error(char *argv[])
 int	cd(char *argv[], t_list *envp[])
 {
 	if (!envp || argv[1] == NULL)
-		return 1;
+		return (1);
+	if (ft_strncmp(argv[1], "~", 2) == 0)
+	{
+		chdir(ms_getenv(envp, "HOME"));
+		return (0);
+	}
 	if (chdir(argv[1]) < 0)
 	{
 		print_cd_error(argv);
 		return (1);
 	}
-	if (ft_strncmp(argv[1], "~", 1) == 0 && cd_home(envp) == 1)
-	{
-		chdir(ms_getenv(envp, "HOME"));
-		return (0);
-	}
-	if (ft_strncmp(argv[1], "..", 2) == 0)
-	{
-		chdir("..");
-		return (0);
-	}
-	if (ft_strncmp(argv[1], ".", 1) == 0)
-		return (0);
-	if (ft_strncmp(argv[1], "/", 1) == 0 && chdir(argv[1]) == -1)
-	{
-		chdir("/");
-		return (0);
-	}
-	chdir(argv[1]);
 	return (0);
 }
