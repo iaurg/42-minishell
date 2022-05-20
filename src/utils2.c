138ac83caa7f44834e3705478a3656a6258dd427
Victor Wildner <vcwildm@gmail.com>
2022-05-20 04:57:19 -0300

    wip: improve lexer

--- src/utils2.c
+++ src/utils2.c
@@ -6,7 +6,7 @@
 /*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 02:27:44 by vwildner          #+#    #+#             */
-/*   Updated: 2022/05/20 03:15:25 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/05/20 04:50:47 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,20 +52,97 @@ void	handle_exit(const char *s)
 	exit(EXIT_FAILURE);
 }
 
+char *replace_explicit_quotes(char *str)
+{
+	int		i;
+	char	*tmp;
+
+	i = -1;
+	tmp = malloc(sizeof(char) * (strlen(str) + 1));
+	while (str[++i + 1])
+	{
+		if ((str[i] == '\\' && (str[i + 1] == '\"' || str[i + 1] == '\'')))
+		{
+			tmp[i] = '#';
+			if (str[i + 1] == '\"')
+				tmp[i + 1] = '^';
+			else if (str[i + 1] == '\'')
+				tmp[i + 1] = '$';
+			i++;
+		}
+		else
+			tmp[i] = str[i];
+	}
+	tmp[i] = str[i];
+	i++;
+	tmp[i] = '\0';
+	return (tmp);
+}
+
+char *clear_quotes(char *str)
+{
+	int i;
+	int j;
+	char *tmp;
+
+	i = -1;
+	j = 0;
+	tmp = malloc(sizeof(char) * (ft_strlen(str) + 1));
+	while (str[++i + 1])
+	{
+		if ((str[i] == '#' && (str[i + 1] == '^' || str[i + 1] == '$')))
+		{
+			if (str[i + 1] == '^')
+				tmp[j] = '\"';
+			else if (str[i + 1] == '$')
+				tmp[j] = '\'';
+			if (str[i + 2])
+				i++;
+		}
+		else
+			tmp[j] = str[i];
+		j++;
+	}
+	tmp[j] = str[i];
+	i++;
+	j++;
+	tmp[j] = '\0';
+	return (tmp);
+}
+
+char **map_clear_quotes(char **args)
+{
+	int i;
+	char	**final;
+
+	i = 0;
+	while (args[i])
+		i++;
+	final = (char **)malloc(sizeof(char *) * (i + 1));
+	i = -1;
+	while (args[++i])
+		final[i] = clear_quotes(args[i]);
+	free_matrix(args);
+	return (final);
+}
+
 int	odd_quote_tokens(char *str, t_command *cmd)
 {
-	int	i;
-	int	j;
-	int	k;
+	int		i;
+	int		j;
+	int		k;
+	char	*tmp;
+	char	**args;
 
 	i = -1;
 	j = 0;
 	k = 0;
-	while (str[++i])
+	tmp = replace_explicit_quotes(str);
+	while (tmp[++i])
 	{
-		if (str[i] == '\'')
+		if (tmp[i] == '\'')
 			j++;
-		if (str[i] == '\"')
+		if (tmp[i] == '\"')
 			k++;
 	}
 	if (j % 2 == 1 || k % 2 == 1)
@@ -75,7 +152,7 @@ int	odd_quote_tokens(char *str, t_command *cmd)
 			STDERR_FILENO);
 		return (1);
 	}
-	strip_quotes(str);
-	cmd->argv = parse_whitespace(buffer, "\'\"");
+	args = parse_whitespace(tmp, "\'\"");
+	cmd->argv = map_clear_quotes(args);
 	return (0);
 }
