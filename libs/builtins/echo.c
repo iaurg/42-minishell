877272cd210c6f24754e577c39459fa0f40937b6
Victor Wildner <vcwildm@gmail.com>
2022-06-01 22:23:05 -0300

    fix: remove commented lines from echo

--- libs/builtins/echo.c
+++ libs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 04:54:03 by itaureli          #+#    #+#             */
-/*   Updated: 2022/05/04 22:54:40 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/06/01 22:22:54 by vwildner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,6 @@ int	echo(char **args, int fd)
 	int i;
 	int has_n;
 
-	//setbuffer(stdout, NULL, 0);
 	i = 1;
 	has_n = check_flag_n(args[i]);
 	if (has_n)
@@ -37,10 +36,7 @@ int	echo(char **args, int fd)
 		i++;
 	}
 	if (has_n)
-	{
-		//fflush(stdout);
 		return (0);
-	}
 	write(fd, "\n", 1);
 	return (0);
 }
