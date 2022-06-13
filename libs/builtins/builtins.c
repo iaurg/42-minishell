9432a5945afd959022a4682598d1fc42b469bc7b
Italo <italoaurelior@gmail.com>
2022-06-13 17:45:45 -0300

    fix: validate exit many args (#89)
    * fix: validate exit many args
    
    * refactor: if logic

--- libs/builtins/builtins.c
+++ libs/builtins/builtins.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 22:23:50 by vwildner          #+#    #+#             */
-/*   Updated: 2022/06/07 18:17:29 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/06/11 17:18:50 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,9 @@ int	builtins_env(t_command *cmd)
 
 int	builtins_exit(t_command *cmd)
 {
-	if (cmd->argc > 1)
+	if (cmd->argc > 2)
+		write(STDERR_FILENO, "exit: too many arguments\n", 26);
+	else if (cmd->argc > 1)
 		exit(ft_atoi(cmd->argv[1]));
 	else
 		exit(0);
