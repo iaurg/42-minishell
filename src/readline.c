f72820e1fbd2c338994cf17b2380d9f38a09ffd7
Italo A <italoaurelior@gmail.com>
2022-06-27 22:32:32 -0300

    fix: remove not used elements

--- src/readline.c
+++ src/readline.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   readline.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vwildner <vwildner@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 16:31:19 by itaureli          #+#    #+#             */
-/*   Updated: 2022/06/07 18:20:01 by vwildner         ###   ########.fr       */
+/*   Updated: 2022/06/27 22:26:20 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,56 +17,6 @@ static void	save_history(char *input)
 	if (input && *input)
 		add_history(input);
 }
-
-char	*get_last_slash_arg(char *arg)
-{
-	char	*args[256];
-	int		i;
-	int		j;
-
-	i = 0;
-	j = 0;
-	while (arg[i])
-	{
-		if (arg[i] == '/')
-		{
-			args[j] = &arg[i + 1];
-			j++;
-		}
-		i++;
-	}
-	return (args[j - 1]);
-}
-
-//static char	*get_inline_shell_display(t_list *envp[])
-//{
-//	char	cwd[1025];
-//	char	hostname[257];
-//	char	*tmp;
-//	char	*other;
-//	char	*path;
-//	char	*tmp_user;
-
-//	getcwd(cwd, 1024);
-//	gethostname(hostname, 256);
-//	path = get_last_slash_arg(cwd);
-//	tmp = ft_strdup("\033[1;31m");
-//	tmp_user = ft_strdup(ms_getenv(envp, "USER"));
-//	other = ft_strjoin(tmp, tmp_user);
-//	free(tmp);
-//	tmp = ft_strjoin(other, "\033[0;36m@");
-//	free(other);
-//	other = ft_strjoin(tmp, hostname);
-//	free(tmp);
-//	tmp = ft_strjoin(other, " \033[0;34m");
-//	free(other);
-//	other = ft_strjoin(tmp, path);
-//	free(tmp);
-//	tmp = ft_strjoin(other, " ~ $\033[0m ");
-//	free(other);
-//	free(tmp_user);
-//	return (tmp);
-//}
 
 int	take_input(char *buffer, t_command *cmd)
 {
